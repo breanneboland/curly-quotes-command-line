999586ce3e846c78c77e243a02b9c8630c351792
Breanne Boland <breanneboland@gmail.com>
2016-08-25 17:29:46 -0700

    tool version using input() from command line

--- command-line-quote-fixer.py
+++ command-line-quote-fixer.py
@@ -1,24 +1,41 @@
-import sys
+fixed_string_list = []
+problem_pointer_list = []
 
-print(sys.argv)
-string_input = sys.argv[1]
-print("You entered", string_input)
+magical_character_conversion_dict = {
+    "“": "\"",
+    "”": "\"",
+    "‘": "\'",
+    "’": "\'"
+}
 
-def unfuck_your_string (str):
-    global string_judgment
+def judge_your_string(string_list):
     string_judgment = None
-    if "“" in str:
-        string_judgment = True
-    elif "”" in str:
-        string_judgment = True
-    elif "‘" in str:
-        string_judgment = True
-    elif "’" in str:
-        string_judgment = True
+    for char in string_to_list:
+        if char in magical_character_conversion_dict.keys():
+            return True
+        else:
+            pass
+    return False
+
+def fix_your_string(string_list):
+    for char in string_list:
+        if char in magical_character_conversion_dict.keys():
+            swap_char = magical_character_conversion_dict[char]
+            fixed_string_list.append(swap_char)
+            problem_pointer_list.append("^")
+        else:
+            fixed_string_list.append(char)
+            problem_pointer_list.append(" ")
+
+if __name__ == "__main__":
+    string_input = input("Paste your questionable string here: ")
+    string_to_list = list(string_input)
+
+    if not string_input:
+        print("No ideas? Run this again and try this: “Riddled with smartquotes, this.”")
+    elif judge_your_string(string_input) == True:
+        fix_your_string(string_to_list)
+        print("ftfy:", "".join(fixed_string_list))
+        print("     ", "".join(problem_pointer_list))
     else:
-        string_judgment = False
-
-
-
-unfuck_your_string(string_input)
-print(string_judgment)
+        print("Looks great. Smooth sailing.")
